a8959f63bd1a4a0e102b0029ed0d9d4530f42c6f
Reyshyram <102749075+Reyshyram@users.noreply.github.com>
2026-02-22 16:37:53 +0100

    Fix: remove implicit memset used by clang

--- lib/my/list/my_sort_list.c
+++ lib/my/list/my_sort_list.c
@@ -27,9 +27,11 @@ static linked_list_t *separate_in_half(linked_list_t *head)
 static linked_list_t *merge(linked_list_t *head, linked_list_t *middle,
     int (*cmp)(const void *, const void *, void *), void *arg)
 {
-    linked_list_t start = {nullptr, nullptr};
+    linked_list_t start;
     linked_list_t *tmp = &start;
 
+    start.data = nullptr;
+    start.next = nullptr;
     while (head && middle) {
         if (cmp(head->data, middle->data, arg) <= 0) {
             tmp->next = head;
