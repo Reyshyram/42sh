207c892451b95eb21d647ff536a81faacb1d765a
Alexandre De Seixas <alexandre.de-seixas@epitech.eu>
2026-04-22 09:31:51 +0200

    Feat: added slightly more tests

---
+++ tests/builtins/test_42sh_cd.c
@@ -0,0 +1,162 @@
+/*
+** EPITECH PROJECT, 2026
+** 42sh
+** File description:
+** simple_test
+*/
+
+
+#include "builtins.h"
+#include "my/list.h"
+#include "my/strings.h"
+#include "shell.h"
+#include <criterion/criterion.h>
+#include <criterion/internal/assert.h>
+#include <criterion/redirect.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "env.h"
+#include <signal.h>
+#include <time.h>
+#include <unistd.h>
+#include "my/list.h"
+#include "my/misc.h"
+
+#include "ast.h"
+#include "env.h"
+#include "executer.h"
+#include "lexer.h"
+#include "parser.h"
+#include "shell.h"
+
+
+static void handle_sigint([[maybe_unused]] int signal)
+{
+    write(STDOUT_FILENO, "\n", 1);
+}
+
+static bool init_variables(shell_t *shell)
+{
+    char *home = get_variable_value(shell->env, "HOME");
+    char *cwd = getcwd(nullptr, 0);
+
+    if (!cwd)
+        return false;
+    if (home && !set_variable(&shell->variables, "home", home))
+        return false;
+    if (!set_variable(&shell->variables, "cwd", cwd))
+        return false;
+    free(cwd);
+    return true;
+}
+
+static bool init_shell(shell_t *shell, char **env)
+{
+    shell->last_status = 0;
+    shell->interactive = isatty(STDIN_FILENO);
+    shell->should_exit = false;
+    shell->is_subprocess = false;
+    shell->is_out_redirected = false;
+    shell->is_in_redirected = false;
+    shell->env = env_to_list(env);
+    shell->variables = nullptr;
+    if (!shell->env && *env)
+        return false;
+    if (shell->interactive && signal(SIGINT, handle_sigint) == SIG_ERR)
+        return false;
+    return init_variables(shell);
+}
+
+Test(working_cd, easy)
+{
+    shell_t shell;
+    bool our_env;
+    bool not_our_env;
+    char **env = __environ;
+    char *av[2] = {"cd", ".."};
+
+    init_shell(&shell, env);
+    cr_redirect_stdout();
+    our_env = builtin_cd(&shell, 2, av);
+    not_our_env = SUCCESS;
+    cr_assert_eq(our_env, not_our_env);
+}
+
+Test(not_working_cd, easy)
+{
+    shell_t shell;
+    bool our_env;
+    bool not_our_env;
+    char **env = __environ;
+
+    init_shell(&shell, env);
+    cr_redirect_stdout();
+    our_env = builtin_cd(&shell, 3, NULL);
+    not_our_env = ERROR;
+    cr_assert_eq(our_env, not_our_env);
+}
+
+Test(cd_home, medium)
+{
+    shell_t shell;
+    bool our_env;
+    bool not_our_env;
+    char **env = __environ;
+
+    init_shell(&shell, env);
+    cr_redirect_stdout();
+    our_env = builtin_cd(&shell, 1, NULL);
+    not_our_env = SUCCESS;
+    cr_assert_eq(our_env, not_our_env);
+}
+
+Test(no_home_cd, medium)
+{
+    shell_t shell;
+    bool our_env;
+    bool not_our_env;
+    char **env = __environ;
+    char *av[2] = {"unsetenv", "HOME"};
+    char *av2[2] = {"unsetenv", "home"};
+
+    init_shell(&shell, env);
+    builtin_unsetenv(&shell, 2, av);
+    builtin_unset(&shell, 2, av2);
+    cr_redirect_stdout();
+    our_env = builtin_cd(&shell, 1, NULL);
+    not_our_env = ERROR;
+    cr_assert_eq(our_env, not_our_env);
+}
+
+Test(cd_to_non_existant, medium)
+{
+    shell_t shell;
+    bool our_env;
+    bool not_our_env;
+    char **env = __environ;
+    char *av2[2] = {"cd", "libsqd"};
+
+    init_shell(&shell, env);
+    cr_redirect_stdout();
+    our_env = builtin_cd(&shell, 2, av2);
+    not_our_env = ERROR;
+    cr_assert_eq(our_env, not_our_env);
+}
+
+Test(cd_to_previous, hard)
+{
+    shell_t shell;
+    bool our_env;
+    bool not_our_env;
+    char **env = __environ;
+    char *av[2] = {"cd", ".."};
+    char *av2[2] = {"cd", "-"};
+
+    init_shell(&shell, env);
+    cr_redirect_stdout();
+    builtin_cd(&shell, 2, av);
+    our_env = builtin_cd(&shell, 2, av2);
+    not_our_env = SUCCESS;
+    cr_assert_eq(our_env, not_our_env);
+}
---
+++ tests/executer/test_42sh_executer.c
@@ -0,0 +1,311 @@
+/*
+** EPITECH PROJECT, 2026
+** 42sh
+** File description:
+** simple_test
+*/
+
+
+#include "builtins.h"
+#include "my/list.h"
+#include "my/strings.h"
+#include "shell.h"
+#include <criterion/criterion.h>
+#include <criterion/internal/assert.h>
+#include <criterion/redirect.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "env.h"
+#include <signal.h>
+#include <time.h>
+#include <unistd.h>
+#include "my/list.h"
+#include "my/misc.h"
+
+#include "ast.h"
+#include "env.h"
+#include "executer.h"
+#include "lexer.h"
+#include "parser.h"
+#include "shell.h"
+#include "token.h"
+
+
+static void handle_sigint([[maybe_unused]] int signal)
+{
+    write(STDOUT_FILENO, "\n", 1);
+}
+
+static bool init_variables(shell_t *shell)
+{
+    char *home = get_variable_value(shell->env, "HOME");
+    char *cwd = getcwd(nullptr, 0);
+
+    if (!cwd)
+        return false;
+    if (home && !set_variable(&shell->variables, "home", home))
+        return false;
+    if (!set_variable(&shell->variables, "cwd", cwd))
+        return false;
+    free(cwd);
+    return true;
+}
+
+static bool init_shell(shell_t *shell, char **env)
+{
+    shell->last_status = 0;
+    shell->interactive = isatty(STDIN_FILENO);
+    shell->should_exit = false;
+    shell->is_subprocess = false;
+    shell->is_out_redirected = false;
+    shell->is_in_redirected = false;
+    shell->env = env_to_list(env);
+    shell->variables = nullptr;
+    if (!shell->env && *env)
+        return false;
+    if (shell->interactive && signal(SIGINT, handle_sigint) == SIG_ERR)
+        return false;
+    return init_variables(shell);
+}
+
+Test(execute_single_ls, easy)
+{
+    shell_t shell;
+    lexer_t lexer;
+    parser_t parser;
+    ast_node_t *node;
+    int our;
+    char **env = __environ;
+
+    init_shell(&shell, env);
+    cr_redirect_stdout();
+    lexer_init(&lexer, "ls", &shell);
+    parser_init(&parser, &lexer);
+    node = parse_command(&parser);
+    our = execute_cmd(&shell, node);
+    cr_assert_eq(our, SUCCESS);
+}
+
+Test(execute_multiple_ls, easy)
+{
+    shell_t shell;
+    lexer_t lexer;
+    parser_t parser;
+    ast_node_t *node;
+    int our;
+    char **env = __environ;
+
+    init_shell(&shell, env);
+    cr_redirect_stdout();
+    lexer_init(&lexer, "ls;ls;ls;ls", &shell);
+    parser_init(&parser, &lexer);
+    node = parser_parse(&parser, false);
+    our = execute_ast(&shell, node);
+    cr_assert_eq(our, SUCCESS);
+}
+
+Test(execute_piped_ls, easy)
+{
+    shell_t shell;
+    lexer_t lexer;
+    parser_t parser;
+    ast_node_t *node;
+    int our;
+    char **env = __environ;
+
+    init_shell(&shell, env);
+    cr_redirect_stdout();
+    lexer_init(&lexer, "ls | ls > ha| ls", &shell);
+    parser_init(&parser, &lexer);
+    node = parser_parse(&parser, false);
+    our = execute_pipe(&shell, node);
+    cr_assert_eq(our, SUCCESS);
+}
+
+Test(execute_redirected_ls, easy)
+{
+    shell_t shell;
+    lexer_t lexer;
+    parser_t parser;
+    ast_node_t *node;
+    int our;
+    char **env = __environ;
+
+    init_shell(&shell, env);
+    cr_redirect_stdout();
+    lexer_init(&lexer, "ls > ha", &shell);
+    parser_init(&parser, &lexer);
+    node = parser_parse(&parser, false);
+    our = execute_ast(&shell, node);
+    cr_assert_eq(our, SUCCESS);
+}
+
+Test(execute_null, easy)
+{
+    shell_t shell;
+    lexer_t lexer;
+    parser_t parser;
+    ast_node_t *node;
+    int our;
+    char **env = __environ;
+
+    init_shell(&shell, env);
+    cr_redirect_stdout();
+    lexer_init(&lexer, "ls > ha; foo < ha", &shell);
+    parser_init(&parser, &lexer);
+    our = execute_ast(&shell, NULL);
+    cr_assert_eq(our, SUCCESS);
+}
+
+Test(execute_complex_command, hard)
+{
+    shell_t shell;
+    lexer_t lexer;
+    parser_t parser;
+    ast_node_t *node;
+    int our;
+    char **env = __environ;
+
+    init_shell(&shell, env);
+    cr_redirect_stdout();
+    lexer_init(&lexer, "ls; ls > ha && cat ha; true || false; /bin/ls > boo", &shell);
+    parser_init(&parser, &lexer);
+    node = parser_parse(&parser, false);
+    our = execute_ast(&shell, node);
+    cr_assert_eq(our, SUCCESS);
+}
+
+Test(execute_failed_left, easy)
+{
+    shell_t shell;
+    lexer_t lexer;
+    parser_t parser;
+    ast_node_t *node;
+    int our;
+    char **env = __environ;
+
+    init_shell(&shell, env);
+    cr_redirect_stdout();
+    lexer_init(&lexer, "thingymajib || ls", &shell);
+    parser_init(&parser, &lexer);
+    node = parser_parse(&parser, false);
+    our = execute_ast(&shell, node);
+    cr_assert_eq(our, SUCCESS);
+}
+
+Test(execute_success_left, easy)
+{
+    shell_t shell;
+    lexer_t lexer;
+    parser_t parser;
+    ast_node_t *node;
+    int our;
+    char **env = __environ;
+
+    init_shell(&shell, env);
+    cr_redirect_stdout();
+    lexer_init(&lexer, "ls && ls", &shell);
+    parser_init(&parser, &lexer);
+    node = parser_parse(&parser, false);
+    our = execute_ast(&shell, node);
+    cr_assert_eq(our, SUCCESS);
+}
+
+Test(execute_redir_into_existing_file, easy)
+{
+    shell_t shell;
+    lexer_t lexer;
+    parser_t parser;
+    ast_node_t *node;
+    int our;
+    char **env = __environ;
+
+    init_shell(&shell, env);
+    cr_redirect_stdout();
+    lexer_init(&lexer, "ls > src/main.c", &shell);
+    parser_init(&parser, &lexer);
+    node = parser_parse(&parser, true);
+    our = execute_subshell(&shell, node);
+    cr_assert_eq(our, SUCCESS);
+}
+
+Test(execute_env, easy)
+{
+    shell_t shell;
+    lexer_t lexer;
+    parser_t parser;
+    ast_node_t *node;
+    int our;
+    char **env = __environ;
+
+    init_shell(&shell, env);
+    cr_redirect_stdout();
+    lexer_init(&lexer, strdup("env"), &shell);
+    parser_init(&parser, &lexer);
+    node = parser_parse(&parser, true);
+    if (!node)
+        return;
+    our = execute_cmd(&shell, node);
+    cr_assert_eq(our, SUCCESS);
+}
+
+Test(execute_wrong_command, easy)
+{
+    shell_t shell;
+    lexer_t lexer;
+    parser_t parser;
+    ast_node_t *node;
+    int our;
+    char **env = __environ;
+
+    init_shell(&shell, env);
+    cr_redirect_stdout();
+    lexer_init(&lexer, strdup("ibuihbjj"), &shell);
+    parser_init(&parser, &lexer);
+    node = parser_parse(&parser, true);
+    if (!node)
+        return;
+    our = execute_cmd(&shell, node);
+    cr_assert_eq(our, ERROR);
+}
+
+Test(execute_folder, easy)
+{
+    shell_t shell;
+    lexer_t lexer;
+    parser_t parser;
+    ast_node_t *node;
+    int our;
+    char **env = __environ;
+
+    init_shell(&shell, env);
+    cr_redirect_stdout();
+    lexer_init(&lexer, strdup("/bin"), &shell);
+    parser_init(&parser, &lexer);
+    node = parser_parse(&parser, true);
+    if (!node)
+        return;
+    our = execute_cmd(&shell, node);
+    cr_assert_eq(our, ERROR);
+}
+
+Test(execute_cat_on_no_perms_file, easy)
+{
+    shell_t shell;
+    lexer_t lexer;
+    parser_t parser;
+    ast_node_t *node;
+    int our;
+    char **env = __environ;
+
+    init_shell(&shell, env);
+    cr_redirect_stdout();
+    lexer_init(&lexer, strdup("cat no_perms"), &shell);
+    parser_init(&parser, &lexer);
+    node = parser_parse(&parser, true);
+    if (!node)
+        return;
+    our = execute_cmd(&shell, node);
+    cr_assert_eq(our, ERROR);
+}
