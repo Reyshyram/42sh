1ecd4cc8f4bf1bc544d2a5cf56b52b2c56d39ce9
Adélaïde François <adelaide1.francois@epitech.eu>
2026-04-27 13:04:01 +0200

    feat: doxygen format for src/lexer/tokens/ files | part.1

--- src/lexer/tokens/lexer_logical_and.c
+++ src/lexer/tokens/lexer_logical_and.c
@@ -8,6 +8,13 @@
 #include "lexer.h"
 #include "token.h"
 
+/*************************************
+* The lexer_logical_and function adds an "and" for 42sh.
+* It respects the Banana and epiclang coding styles from Epitech.
+*
+*   @param -> lexer_t *lexer, a structure found in include/lexer.h
+*   @return -> a new token, LOGICAL_AND
+*************************************/
 token_t *lexer_logical_and(lexer_t *lexer)
 {
     lexer->pos += 2;
