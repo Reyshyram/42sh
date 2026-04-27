4e33843861ceb159d0d4304fafd25d9cc0040bdf
Adélaïde François <adelaide1.francois@epitech.eu>
2026-04-27 12:40:33 +0200

    feat: doxygen format for src/variables/ files

--- src/variables/get_variable_value.c
+++ src/variables/get_variable_value.c
@@ -9,6 +9,14 @@
 
 #include "shell.h"
 
+/*************************************
+* The get_variable_value function gets the value of a data for 42sh.
+* It respects the Banana and epiclang coding styles from Epitech.
+*
+*   @param -> linked_list_t *variables, structure found in include/shell.h
+*   @param -> char *key, a string of the key
+*   @return -> either the value or nullptr
+*************************************/
 char *get_variable_value(linked_list_t *variables, char *key)
 {
     variable_t *data = get_variable(variables, key);
